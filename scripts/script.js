b668fe3f970c3b9c971b70f8612c9c605c12971d
JaafarHawli <jaafarhawle@gmail.com>
2022-09-15 01:40:07 +0300

    added sign in modal functionality

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,11 @@ const quit = document.getElementById('quit');
 const signup_modal = document.getElementById('signup_modal');
 const signup_next = document.getElementById('next');
 const signup_modal_password = document.getElementById('signup_modal_password');
+const signin = document.getElementById('signin');
+const signin_modal = document.getElementById('signin_modal');
+const signin_quit = document.getElementById('signin_quit');
 const back = document.getElementById('back');
+const effect = document.getElementById('quit_effect');
 
 signup.addEventListener('click', () => {
 	signup_modal.showModal();
@@ -26,3 +30,21 @@ back.addEventListener('click', () => {
 	signup_modal_password.close();
 	signup_modal.showModal();
 });
+
+signin.addEventListener('click', () => {
+	signin_modal.showModal();
+	document.body.style.overflow = 'hidden';
+	document.body.style.userSelect = 'none';
+});
+
+signin_quit.addEventListener('click', () => {
+	signin_modal.close();
+	document.body.style.overflow = 'auto';
+	document.body.style.userSelect = 'auto';
+});
+
+/* effect.addEventListener('mouseover', () => {
+	effect.style.background = 'black';
+	effect.style.opacity = 0.3;
+});
+ */
